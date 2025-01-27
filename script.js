5f5a3ac76e6cf763134e32807cf4e2d04bd0fcd6
Saihariram <sairamv_2002@yahoo.com>
2025-01-27 17:14:56 +0530

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('contactForm').addEventListener('submit', function (e) {
-    e.preventDefault();
-    alert('Thank you for reaching out to us! We will get back to you soon.');
-});
